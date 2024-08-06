6ffcda4e7efded07ec2fee0b1cb2a8d0cc655e67
MINJ HWNAG <minji_hwang@sawwave.kr>
2024-08-06 17:42:45 +0900

    https://github.com/hwang8596/myapp

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//const About = () => {
+function About() {
+    return (
+        <div>
+            <h1>About 화면입니다.</h1>
+        </div>
+    )
+}
+
+export default About
